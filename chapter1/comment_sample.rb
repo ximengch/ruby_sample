880dcca18a8bc5f6adf3f2c403741a97ddbe8d43
ximengch <ximengchj@outlook.com>
2015-01-30 15:46:11 +0800

    注释

---
+++ chapter1/comment_sample.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+=begin
+“快乐学ruby第二版”范例
+注释用法的范例
+   20150130建立
+=end
+x=10  #高度
+y=20  #深度
+z=30  #宽度
+#计算表面积
+are=(x*y+x*z+y*z)*2
+volume=x*y*z
+#输出
+print("表面积=",are,"\n")
+print("体积=",volume,"\n")
